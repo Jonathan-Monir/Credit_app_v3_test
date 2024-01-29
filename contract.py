f75ab4a91826b02ff2d71f105cafcfc08eada13a
Jonathan Monir <jonmonir@yahoo.com>
2024-01-29 22:54:58 +0200

    initialize contract class

--- contract.py
+++ contract.py
@@ -1,5 +1,13 @@
 class Contract:
     
-    def __init__(self, input_dict, offers):
-        self.input_dict = input_dict
+    def __init__(self, EarlyBooking1, EarlyBooking2, LongTerm, Reduction1, Reduction2, contract, start_date=None, end_date=None):
+        self.EarlyBooking1 = EarlyBooking1
+        self.EarlyBooking2 = EarlyBooking2
+        self.LongTerm = LongTerm
+        self.Reduction1 = Reduction1
+        self.Reduction2 = Reduction2
+        self.contract = contract
         
+        self.start_date = start_date if start_date is not None else self.contract.loc[0,"first date"]
+        self.end_date = end_date if end_date is not None else self.contract.loc[0,"first date"]
+
--- file_uploader.py
+++ file_uploader.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 import numpy as np
+
 class FileUploader:
     def __init__(self, filepath):
         self.filepath = filepath
