22057b238ae33184c238d4d86a26d109d3ac814a
Jonathan-Monir <jonmonir@yahoo.com>
2024-02-19 17:39:48 +0200

    Update file paths for test data and remove unnecessary code.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def make_contracts_dict(contracts_sheets,contract_activity):
             offers_dict[contract_name] = Contract(contract_name, contract_data, contract_activity[contract_name])
         return offers_dict
 
-df = FileUploader(r"test files\test.xlsx")
+df = FileUploader(r"test files\test2.xlsx")
 statment = df.statment
 
 
@@ -27,11 +27,8 @@ invoice = Invoice(df,offers_dict)
 prices = Invoice(df,offers_dict).prices
 date_prices = Invoice(df,offers_dict).Index_contract_date_range_dict
 
-print(invoice.offers_dict)
 statment.loc[prices.keys(), "Total price"] = [round(price, 2) for price in list(prices.values())]
 
 
 
-statment.to_excel("output.xlsx", index=False)
-
-statment.to_excel("output.xlsx", index=False)
+# statment.to_excel("output.xlsx", index=False)
