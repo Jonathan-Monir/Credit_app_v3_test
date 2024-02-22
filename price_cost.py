6c2ab388c0622765b240d8fc7dc49a041059d9f0
Jonathan Monir <jonmonir@yahoo.com>
2024-02-22 14:05:55 +0200

    delete unimportant files

--- price_cost.py
+++
@@ -1,9 +0,0 @@
-class Price:
-    def __init__(self,Total_price,statment,contract_sheets):
-        self.Total_price = Total_price
-        
-    def valid_days(self):
-        pass
-    
-    def valid_contracts(statment,contract_sheets):
-        pass
